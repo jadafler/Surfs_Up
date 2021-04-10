653fe27af4cc1f3634164e406a628c75e20e5995
jadafler <71464211+jadafler@users.noreply.github.com>
2021-04-10 00:42:40 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -8,9 +8,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
+app = Flask(__name__)
+
 engine = create_engine("sqlite:///hawaii.sqlite")
 
 Base = automap_base()
+
 Base.prepare(engine, reflect=True)
 
 Measurement = Base.classes.measurement
@@ -18,19 +21,16 @@ Station = Base.classes.station
 
 session = Session(engine)
 
-app = Flask(__name__)
-
 @app.route("/")
 def welcome():
     return(
-'''
-Welcome to the Climate Analysis API!
-Available Routes:
-/api/v1.0/precipitation
-/api/v1.0/stations
-/api/v1.0/tobs
-/api/v1.0/temp/start/end
-''')
+        f"Welcome to the Climate Analysis API!</br>"
+        f"Available Routes:</br>"
+        f"/api/v1.0/precipitation</br>"
+        f"/api/v1.0/stations</br>"
+        f"/api/v1.0/tobs</br>"
+        f"/api/v1.0/temp/start/end</br>"
+)
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
